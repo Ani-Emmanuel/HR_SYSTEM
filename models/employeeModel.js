a127ccaeabf68154890f6c742e29e39220b46b7d
Ani-Emmanuel <mailsforanih@gmail.com>
2019-09-18 05:41:07 +0100

    Updated the model

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -8,8 +8,9 @@ const employeeShema = mongoose.Schema(
     dob: { type: Date, required: true },
     address: { type: String, required: true },
     sex: { type: String, required: true },
-    //passport: { data: Buffer, contentType: String },
+    // passport: { data: Buffer, contentType: String },
     dateOfResume: { type: Date, required: true },
+    isActive: { type: Boolean, required: true },
     phone: { type: Number, required: true },
     state: { type: String, required: true },
     religion: { type: String, required: true },
@@ -27,16 +28,34 @@ const account = mongoose.Schema(
     accountName: { type: String, required: true },
     accountNumber: { type: Number, required: true },
     BankName: { type: String, required: true },
-    salary: { type: Number, required: true },
-    deductions: { type: Number, default: 0 },
-    tax: { type: Number, required: true },
-    commission: { type: Number, default: 0 },
-    employee: { type: mongoose.Schema.Types.ObjectId, ref: "Employee", index : true }
+    bankCode: { type: String, required: true },
+    employee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Employee"
+    }
   },
   { timestamps: true }
 );
 
+const salary = mongoose.Schema({
+  deductions: { type: Number, default: 0 },
+  month: { type: String, required: true },
+  tax: { type: Number, required: true },
+  commission: { type: Number, default: 0 },
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Employee"
+  }
+});
+
+const salaryStructure = mongoose.Schema({
+  level: { type: Number, required: true },
+  baseSalary: { type: Number, required: true }
+});
+
+const SalaryStructure = mongoose.model("SalaryStructure", salaryStructure);
+const Salary = mongoose.model("Salary", salary);
 const Account = mongoose.model("Account", account);
 const Employee = mongoose.model("Employee", employeeShema);
 
-module.exports = { Employee, Account };
+module.exports = { Employee, Account, Salary, SalaryStructure };
